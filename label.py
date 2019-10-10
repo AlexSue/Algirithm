412bef156b689d85d22b8f2cc56037d866a99b9e
AlexSue <49713197+AlexSue@users.noreply.github.com>
2019-10-10 18:42:26 +0800

    Update label.py

--- label.py
+++ label.py
@@ -4,3 +4,27 @@ with open(filename) as f:
     label = json.load(f)
 # 遍历列表的每个元素，每个元素是一个GDP数据项
 print(label)
+print('x:')
+for t in label:
+    print(t['x'])
+print('\n')
+
+print('y:')
+for t in label:
+    print(t['y'])
+print('\n')
+
+print('w:')
+for t in label:
+    print(t['w'])
+print('\n')
+
+print('h:')
+for t in label:
+    print(t['h'])
+print('\n')
+
+print('class:')
+for t in label:
+    print(t['class'])
+print('\n')
